2b498ca47e8583c86dfba6126d4c5346a5f730d4
Sudara Fernando <sudara@yahoo.com>
2021-12-11 11:23:12 +1300

    Initial Commit

---
+++ Album Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Album Browser
+//
+//  Created by Sudara Fernando on 11/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
