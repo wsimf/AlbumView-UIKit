4ce1ffd84fbf7ff5c3e200aca06f17ff494f1719
Sudara Fernando <sudara@yahoo.com>
2021-12-14 21:37:07 +1300

    Fixed some formatting issues

--- Album Browser/View Controllers/AlbumsPhotoViewController.swift
+++ Album Browser/View Controllers/AlbumsPhotoViewController.swift
@@ -9,24 +9,24 @@ import Foundation
 import UIKit
 
 class AlbumsPhotoViewController: UIViewController {
-
+	
 	@IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
 	@IBOutlet weak var imageView: UIImageView!
 	@IBOutlet weak var photoTitleLabel: UILabel!
 	@IBOutlet weak var albumTitleLabel: UILabel!
-
+	
 	var selectedAlbumPhoto: AlbumPhoto?
-
+	
 	var albumManager: AlbumManager?
 	var cachedImageManager: CachedImageManager?
-
+	
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
-
+		
 		if let albumPhoto = self.selectedAlbumPhoto, let album = self.albumManager?.findAlbum(with: albumPhoto.albumId) {
 			self.photoTitleLabel.text = albumPhoto.title
 			self.albumTitleLabel.text = album.title
-
+			
 			if let cachedImageManager = self.cachedImageManager {
 				Task {
 					do {
--- Album Browser/View Controllers/AlbumsViewController.swift
+++ Album Browser/View Controllers/AlbumsViewController.swift
@@ -70,8 +70,8 @@ class AlbumsViewController: UIViewController {
 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if segue.identifier == "AlbumDetailsSegue",
-			let detailsVc = segue.destination as? AlbumsThumbnailsViewController,
-			let originatingCell = sender as? UITableViewCell {
+		   let detailsVc = segue.destination as? AlbumsThumbnailsViewController,
+		   let originatingCell = sender as? UITableViewCell {
 
 			detailsVc.albumsManager = self.albumsManager
 			detailsVc.selectedAlbum = self.albumsManager.findAlbum(with: originatingCell.tag)
